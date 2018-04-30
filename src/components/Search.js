04c10866d3aedd539402c681b9af8b06d7479d18
Albert Chang <albertchanged@gmail.com>
2018-04-29 18:57:32 -0700

    Clean up Search.js and try adding suitcss dependencies

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,19 +10,19 @@ class Search extends Component {
     this.props.updateGallery(document.querySelector('.searchInput').value);
   }
   render() {
-   return (
-    <div className="search">
-      <img className="searchIcon" src="http://chittagongit.com//images/search-icon-vector/search-icon-vector-10.jpg" />
-      <input 
-        id="inputBox"
-        className="searchInput" 
-        type="text" 
-        placeholder="Search" 
-        onChange={this.handleSearchChange.bind(this)}
-      />
-    </div>
-  );
-}
+    return (
+      <div className="search">
+        <img className="searchIcon" src="http://chittagongit.com//images/search-icon-vector/search-icon-vector-10.jpg" />
+        <input 
+          id="inputBox"
+          className="searchInput" 
+          type="text" 
+          placeholder="Search" 
+          onChange={this.handleSearchChange.bind(this)}
+        />
+      </div>
+    );
+  }
 }
 
 export default Search;
